96658cc5c1cd981ed3a3d4cdcd9ebe7854111431
smore <smore@qualys.com>
2022-12-06 21:02:49 +0530

    Oops Module Completed

---
+++ oOps/oOpsDemo.py
@@ -0,0 +1,31 @@
+# Classes are user defined blueprints or prototype
+# self keyword is mandatory for calling variable names into method
+# instance and class variables have whole different purpose
+# constructor name should be __init__
+# new keyword is not required when you create object
+
+class Calculator:
+    num = 100   # class variables
+
+    # default constructor
+
+    def __init__(self, a, b):
+        self.firstNumber = a
+        self.secondNumber = b
+        print("I am called automatically when object is created")
+
+    def getData(self):
+        print("Welcome To Calculator")
+
+    def summation(self):
+        return self.firstNumber + self.secondNumber + Calculator.num
+
+
+obj = Calculator(2, 3)
+obj.getData()
+print(obj.summation())
+
+obj1 = Calculator(4, 5)
+obj1.getData()
+print(obj1.summation())
+
---
+++ rwFile/readWrite.py
@@ -0,0 +1,12 @@
+file = open('C:\\Users\\smore\\PycharmProjects\\BasicPython\\rwFile\\test.txt')
+# Read all the contents of file
+# Read n number of characters by passing parameter
+# print(file.read())
+# print(file.read(5))
+
+# Read one single line at a time - readLine()
+
+print(file.readline())
+print(file.readline())
+
+file.close()
